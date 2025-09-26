2ec2b35596ba0ff6ccce31b896cabedbba35c884
Ilya Degtyarenko <is.degtyarenko@gmail.com>
2025-09-26 11:38:40 +0300

    chore: Add Pinia integration and configure state management

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
+import '@/assets/styles/scss/main.scss'
 import i18n from '@/plugins/i18n.ts'
+import pinia from '@/plugins/pinia.ts'
 import router from '@/plugins/router.ts'
 import App from '@/App.vue'
 import { createApp } from 'vue'
@@ -7,6 +9,7 @@ const app = createApp(App)
 
 
 app.use(i18n)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
---
+++ src/plugins/pinia.ts
@@ -0,0 +1,6 @@
+import { createPinia } from 'pinia'
+
+/**
+ * Creates an instance of the Pinia plugin.
+ */
+export default createPinia()
